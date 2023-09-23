7b912d6df8c250b15c6ad434653a9c87b63dfac3
tankner <thomasdankner@gmail.com>
2023-09-23 18:00:12 -0500

    Added folder for booze spawning

---
+++ Game Jam 2023 Unity/Assets/BoozeSpawning/Scripts/BoozeSpawner.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoozeSpawner : MonoBehaviour
+{
+
+
+    [SerializeField] private GameObject pumpkin;  // reference to pumpkin
+    [SerializeField] private int numToSpawn;
+    [SerializeField] private int veloMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < numToSpawn; i++)
+        {
+            Instantiate(pumpkin, new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(0, 5.0f), 0),
+                Quaternion.identity);
+            
+
+        }
+    }
+
+    // Update is called once per frame  
+    /*void Update()
+    {
+        
+    }*/
+}
