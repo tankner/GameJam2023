e6166972712f361dc7ca971cea743799ac3ec767
tankner <tankner@tamu.edu>
2023-11-12 19:13:46 -0600

    Added Bullet tag and enemy spawning

--- Game Jam 2023 Unity/Assets/Scripts/EnemySpawner.cs
+++ Game Jam 2023 Unity/Assets/Scripts/EnemySpawner.cs
@@ -8,15 +8,24 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private GameObject enemy;
     [SerializeField] private int spawnTime = 10;
     // Start is called before the first frame update
+    Queue<GameObject> enemyQueue;
+    GameObject currenemy;
     void Start()
     {
+        enemyQueue = new Queue<GameObject>();
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
     // Update is called once per frame
     void Spawn()
     {
-        Instantiate(enemy, new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(0, 5.0f), 0),
+        currenemy = Instantiate(enemy, new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(0, 5.0f), 0),
                 Quaternion.identity);
+        enemyQueue.Enqueue(currenemy);
+    }
+
+    public void Kill()
+    {
+        Destroy(enemyQueue.Dequeue());
     }
 }
